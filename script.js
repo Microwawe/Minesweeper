135e803942dee2e04e00dc24910e5bc48fc7eae6
Mirko Nowak <microvvavve@gmail.com>
2017-09-13 23:17:19 +0300

    Calculates adjacent bombs and shows it on the clicked box

--- script.js
+++ script.js
@@ -13,31 +13,97 @@ $(document).ready(function() {
 
 	createBoard(minMines, rows, columns, container);
 
-	$('.box').on('click', (e) => {
-		console.log($(e.target));
-		console.log($(e.target).next());
-		console.log($(e.target).prev());
-	})
+	// pommin klikkaaminen
+	$('.bomb').on('click', (e) => {
+		console.log("BOOM");
+		$(e.target).removeClass('box');
+		$(e.target).addClass('box-text');
+		$(e.target).html('X');
+	});
+
+	// muiden ruutujen klikkaaminen
+	$('.box').on('click', bombsNearby);
 
 
-
-})
 
 function createBoard(min, rows, columns, parentDiv) {
 	let html  = '';
 	let mines = 0;
+	let id = 1;
 	for (let i=0; i<rows;i++) {
 		for (let j=0;j<columns;j++) {
 			if(Math.random() < 0.25) {
-				html += '<div class="box bomb"></div>'
+				html += '<div id=' + id + ' class="box bomb"></div>'
 				mines++;
 			} else {
-				html += '<div class="box"></div>' 
+				html += '<div id=' + id + ' class="box"></div>' 
 			}
-			
+			id++;
 		}
 	}
-	console.log("mines " + mines);
+	// console.log("mines " + mines);
 	parentDiv.html('<div class="board"></div>');
 	$('.board').html(html);
 }
+
+function bombsNearby(e) {
+	if($(e.target).hasClass('bomb')) {
+		return;
+	}
+	const minID = 1;
+	const maxID = rows * columns;
+	const current = e.target;
+	const id = parseInt(current.id,10);
+	let topID = id - columns;
+	let botID = id + columns;
+	let bombs = 0;
+
+	let adjacent = [];
+
+	// klikatun yläpuolella oleva rivi
+	if(topID >= minID) {
+		let topLeft = topID - 1;
+		let topRight = topID + 1;
+		if ((topLeft % columns) > 0) {
+			adjacent.push(topLeft);
+		}
+		adjacent.push(topID);
+		if ((topRight % columns) != 1) {
+			adjacent.push(topRight);
+		}
+	}  
+
+	// klikatun viereiset palikat
+	if((id - 1) % columns > 0) {
+		adjacent.push(id-1);
+	}
+	if((id + 1) % columns != 1) {
+		adjacent.push(id+1);
+	}
+
+	// klikatun alapuolella oleva rivi
+	if(botID <= maxID) {
+		let botLeft = botID - 1;
+		let botRight = botID + 1;
+		if ((botLeft % columns) > 0) {
+			adjacent.push(botLeft);
+		}
+		adjacent.push(botID);
+		if ((botRight % columns) != 1) {
+			adjacent.push(botRight);
+		}
+	}
+
+	$.each(adjacent, function(index, id) {
+		if($('#'+id).hasClass('bomb')) {
+			bombs++;
+		}
+	})
+	console.log(adjacent);
+	console.log("bombs " + bombs);
+	$(current).removeClass('box');
+	$(current).addClass('box-text');
+	$(current).html(bombs);
+}
+
+})
